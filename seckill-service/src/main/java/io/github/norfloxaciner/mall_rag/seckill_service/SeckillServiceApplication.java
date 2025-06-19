22c994669d717d1d0e223c1b0135cde31f1d6ecc
Rupert-WLLP-Bai <cwllp1230@gmail.com>
2025-06-19 21:44:30 +0800

    seckill-service init

---
+++ seckill-service/src/main/java/io/github/norfloxaciner/mall_rag/seckill_service/SeckillServiceApplication.java
@@ -0,0 +1,11 @@
+package io.github.norfloxaciner.mall_rag.seckill_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillServiceApplication.class, args);
+    }
+}
